38c31a4714c9aa006dea40e91eaedcf58b1e59b9
daudb6 <daudbasharat6@gmail.com>
2024-01-12 23:56:14 +0500

    set up repositery for Math megician

---
+++ src/Component/Herosection/HeroSection.js
@@ -0,0 +1,12 @@
+function HeroSection() {
+  return(
+    <div>
+      <div className="hero-section">
+        <h1>Ustaad G</h1>
+        <p>Shagird</p>
+        <button className="btn">MUREED HU WO</button>
+      </div>
+    </div>
+  )
+}
+export default HeroSection;
