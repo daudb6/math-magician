395ea8fa7786f64354e5cfb3797f5cf5773a4c36
daudb6 <daudbasharat6@gmail.com>
2024-01-15 22:23:19 +0500

    set up repositery

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
-import HeroSection from "./Component/Herosection/HeroSection";
-import Navbar from "./Component/Navbar/Navbar";
-import './App.css';
-
 function App() {
   return (
     <>
-    <Navbar />
-    <HeroSection />
+ 
 
 </>
   )
